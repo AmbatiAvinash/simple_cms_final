36678e52958869b19c196760f1a70a155eb87f8a
AmbatiA <avi.ambati07@gmail.com>
2019-03-31 16:50:03 +0530

    Sunday commit

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -1,48 +1,69 @@
 class SubjectsController < ApplicationController
-  
- 
-  layout false
+
+  layout "admin"
 
   def index
     @subjects = Subject.sorted
   end
 
-  def create
-    @subject = Subject.new(subject_params)
-
-    if @subject.save
-      redirect_to(:action => 'index')
-    else
-      render('new')
-    end
-
-  end
-
   def show
-    @subject = Subject.find_by_id(params[:id])
+    @subject = Subject.find(params[:id])
   end
 
   def new
-    @subject = Subject.new
+    @subject = Subject.new({:name => "Default"})
+  end
+
+  def create
+    # Instantiate a new object using form parameters
+    @subject = Subject.new(subject_params)
+    # Save the object
+    if @subject.save
+      # If save succeeds, redirect to the index action
+      flash[:notice] = "Subject created successfully."
+      redirect_to(:action => 'index')
+    else
+      # If save fails, redisplay the form so user can fix problems
+      render('new')
+    end
   end
 
   def edit
+    @subject = Subject.find(params[:id])
+  end
+
+  def update
+    # Find an existing object using form parameters
+    @subject = Subject.find(params[:id])
+    # Update the object
+    if @subject.update_attributes(subject_params)
+      # If update succeeds, redirect to the index action
+      flash[:notice] = "Subject updated successfully."
+      redirect_to(:action => 'show', :id => @subject.id)
+    else
+      # If update fails, redisplay the form so user can fix problems
+      render('edit')
+    end
   end
 
   def delete
     @subject = Subject.find(params[:id])
-    if @subject.destroy
-      redirect_to(:action => 'index')
-    else
-      redirect_to(:action => 'index')
-    end 
   end
+
+  def destroy
+    subject = Subject.find(params[:id]).destroy
+    flash[:notice] = "Subject '#{subject.name}' destroyed successfully."
+    redirect_to(:action => 'index')
+  end
+
 
   private
 
-  def subject_params
-    params.require(:subject).permit(:name, :position, :visible)
-  end
-
+    def subject_params
+      # same as using "params[:subject]", except that it:
+      # - raises an error if :subject is not present
+      # - allows listed attributes to be mass-assigned
+      params.require(:subject).permit(:name, :position, :visible)
+    end
 
 end
